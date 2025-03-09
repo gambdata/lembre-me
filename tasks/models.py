0516eabe61843195784c80c387de0a4ec41fd623
Paulo Martins <p.o.marttins@gmail.com>
2025-03-09 07:41:55 -0300

    Create Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,18 +1,12 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    new = models.BooleanField(default=True)
+    in_progress = models.BooleanField(default=False)
+    testing = models.BooleanField(default=False)
+    homologation = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
-# class Post(models.Model):
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=200)
-#     text = models.TextField()
-#     created_date = models.DateTimeField(default=timezone.now)
-#     published_date = models.DateTimeField(blank=True, null=True)
-
-#     def publish(self):
-#         self.published_date = timezone.now()
-#         self.save()
-
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
