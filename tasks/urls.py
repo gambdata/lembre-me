e9ab7ea666416d7a0843f80ba959804b9099d0f4
Paulo Martins <p.o.marttins@gmail.com>
2025-03-09 09:04:38 -0300

    New path include

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('update-task/<str:pk>/', views.updateTask, name="update-task")
+    path('update-task/<str:pk>/', views.updateTask, name="update-task"),
+    path('delete-task/<str:pk>/', views.deleteTask, name="delete-task")
 ]
