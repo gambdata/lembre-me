30314b10cd22c1928a201323a096f597662a5828
Paulo Martins <p.o.marttins@gmail.com>
2025-03-09 07:42:44 -0300

    New urls index, update-task

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update-task/<str:pk>/', views.updateTask, name="update-task")
 ]
