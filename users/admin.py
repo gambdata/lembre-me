51c75912867473fe541fc0fd8b5dc4237c1735f8
paullomarttins <p.o.marttins@gmail.com>
2025-05-23 13:15:45 -0300

    Ajustes de model

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from .models import NewUser
+from .models import Usuario
 from django.contrib.auth.admin import UserAdmin
 from django.forms import TextInput, Textarea
 
 class UserAdminConfig(UserAdmin):
-    model = NewUser
+    model = Usuario
     search_fields = ('email', 'user_name', 'first_name',)
     list_filter = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
     ordering = ('-start_date',)
     list_display = ('email', 'user_name', 'first_name', 'is_active', 'is_staff')
     fieldsets = (
         (None, {'fields': ('email', 'user_name', 'first_name',)}),
-        ('Permissions', {'fields': ('is_active', 'is_staff')}),
-        ('Personal', {'fields': ('about',)}),
+        ('Permissões', {'fields': ('is_active', 'is_staff')}),
+        ('Pessoal', {'fields': ('about',)}),
     )
     formfield_overrides = {
-        NewUser.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
+        Usuario.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
     }
     add_fieldsets = (
         (None, {
@@ -24,4 +24,4 @@ class UserAdminConfig(UserAdmin):
         }),
     )
 
-admin.site.register(NewUser, UserAdminConfig)
+admin.site.register(Usuario, UserAdminConfig)
