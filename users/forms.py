3c7448bf4e31dce2b0a0e3d0aaef2e576d963573
paullomarttins <p.o.marttins@gmail.com>
2025-05-23 13:16:08 -0300

    Alterado  model

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import NewUser
+from .models import Usuario
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
-        model = NewUser
+        model = Usuario
         fields = ('user_name', 'email', 'password1', 'password2')
+        widgets = {
+            'user_name': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'name@company.com'})
+        }
