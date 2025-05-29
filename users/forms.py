d37aaa8803dadca1dada2314287f85a23a556606
Paulo Martins <p.o.marttins@gmail.com>
2025-05-28 21:53:35 -0300

    Alterações forms de registro e login

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import NewUser
+from django import forms
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
 
-class RegisterUserForm(UserCreationForm):
+from .models import Usuario
+
+class CustomUserCreationForm(AdminUserCreationForm):
+
     class Meta:
-        model = NewUser
-        fields = ('user_name', 'email', 'password1', 'password2')
+        model = Usuario
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'name@company.com'})
+        }
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Usuario
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'name@company.com'})
+        }
