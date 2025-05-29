1c5285d695e668e62f8ad8a7d06d57367ca3fcdc
Paulo Martins <p.o.marttins@gmail.com>
2025-05-28 21:26:15 -0300

    Classe AbastractUser

--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    pass
+class Usuario(AbstractUser):
+    email = models.EmailField(_('Email'), unique=True)
+    username = models.CharField(_('Usuário'), max_length=150, unique=True)
+
+    #USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
