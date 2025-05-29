cd274ca2894fcf6dbe48d4dd9dcd7e8142ad7ecb
Paulo Martins <p.o.marttins@gmail.com>
2025-05-28 21:25:26 -0300

    Alteração de classes forms

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
+from .models import Usuario
 
-class RegisterUserForm(UserCreationForm):
+class CustomUserCreationForm(AdminUserCreationForm):
+
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password1', 'password2')
+        model = Usuario
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'name@company.com'})
+        }
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Usuario
+        fields = ('username', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'name@company.com'})
+        }
