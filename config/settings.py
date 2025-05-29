0c27a0360cc1c44d5e7d2e32e07d62a9a59d59d2
Paulo Martins <p.o.marttins@gmail.com>
2025-05-28 21:51:42 -0300

    Alteração AUTH_USER_MODEL e REDIRECT

--- config/settings.py
+++ config/settings.py
@@ -130,5 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.NewUser'
-LOGIN_REDIRECT_URL = '/'
+AUTH_USER_MODEL = 'users.Usuario'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
