0ec2e70d874a07a9c47a2e5a7ae02cb8a55a3f41
Paulo Martins <p.o.marttins@gmail.com>
2025-05-30 18:59:43 -0300

    Criado class model para Tickets

--- planner/models.py
+++ planner/models.py
@@ -1,5 +1,27 @@
-# from django.db import models
-# from users.models import CustomUser
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# class Tickets(models.Model):
-    
+from users.models import Usuario
+
+PROGRESS = [
+    ('0', 'Novo'),
+    ('1', 'Em andamento'),
+    ('2', 'Pendente'),
+    ('3', 'Cancelado'),
+    ('4', 'Concluído')
+]
+
+class Ticket(models.Model):
+    criado_por = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tickets_criados')
+    titulo = models.CharField(_('Título'), max_length=50, null=False)
+    conteudo = models.TextField(_('Conteúdo'))
+    progresso = models.CharField(_('Progresso'), max_length=1, choices=PROGRESS, default='0')
+    dt_inicio = models.DateTimeField(_('Data início'), auto_now_add=True)
+    dt_fim = models.DateTimeField(_('Data fim'), blank=True, null=True)
+    dt_conclusao_prev = models.DateTimeField(_('Data conclusão prevista'), blank=True, null=True)
+    dt_atualizacao = models.DateTimeField(_('Data atualização'), auto_now=True)
+    responsavel = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tickets_designados')
+
+    def __str__(self):
+        return self.titulo
