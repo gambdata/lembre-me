2240b1d701f73932fa04d5f294f3b049a0517140
Paulo Martins <p.o.marttins@gmail.com>
2025-06-05 00:09:42 -0300

    Customização do Admin

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.forms import Textarea
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Usuario
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+class UserAdminConfig(UserAdmin):
     model = Usuario
+    search_fields = ('email', 'username', 'first_name',)
+    list_filter = ('email', 'username', 'first_name', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
     list_display = ['email', 'username', 'is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'first_name',)}),
+        ('Permissão', {'fields': ('is_active', 'is_staff')}),
+        ('Pessoal', {'fields': ('about',)})
+    )
+    formfield_overrides = {
+        Usuario.about: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
+    }
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
 
-admin.site.register(Usuario, CustomUserAdmin)
+admin.site.register(Usuario, UserAdminConfig)
