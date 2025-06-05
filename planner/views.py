2ff46f1efd614e3a3febac43edd73f61f77669e7
Paulo Martins <p.o.marttins@gmail.com>
2025-06-05 00:06:54 -0300

    Criada view NovoViewTicket

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import Ticket
+from .forms import NovoTicketForm
+
+class NovoViewTicket(CreateView):
+    model = Ticket
+    form_class = NovoTicketForm
+    template_name = 'planner/novo_ticket.html'
+    success_url = '/planner/'
