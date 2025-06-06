1d2da9e23fe36d3a12a39af71e3ca70f770a2ad2
PAULO OLIVEIRA MARTINS <OMPAULO@brasilcenter.com.br>
2025-06-06 15:46:46 -0300

    Inclusão de editar e deletar

--- planner/urls.py
+++ planner/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import NovoViewTicket
+from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket
 
 urlpatterns = [
     path('criar/', NovoViewTicket.as_view(), name='novo-ticket'),
+    path('editar/<pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
+    path('excluir/<pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
 ]
