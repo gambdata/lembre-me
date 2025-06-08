ef49ef54b33574794a6a31bf3624347b16800a94
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 13:51:45 -0300

    Alteração na estrutura de url index

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import Index
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', Index.as_view(), name='index'),
 ]
