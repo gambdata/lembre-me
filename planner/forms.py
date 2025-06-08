8dd473d50ba2ca783117f700279942529b84c686
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 13:53:26 -0300

    Adicionado widgets para colunas

--- planner/forms.py
+++ planner/forms.py
@@ -5,23 +5,23 @@ from .models import Ticket
 class NovoTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['criado_por','titulo','conteudo','dt_conclusao_prev','responsavel']
+        fields = ['titulo','conteudo','dt_conclusao_prev','responsavel']
         widgets = {
-            'criado_por': forms.Select(attrs={'class': 'form-control'}),
+            # 'criado_por': forms.Select(attrs={'class': 'form-control'}),
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'conteudo': forms.Textarea(attrs={'class': 'form-control'}),
             'dt_conclusao_prev': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'responsavel': forms.Select(attrs={'class': 'form-control'})
         }
 
-    def form_valid(self, form):
-        form.instance.criado_por = self.request.user
-        return super().form.valid(form)
-
 class UpdateTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['titulo','conteudo','progresso','dt_conclusao_prev','responsavel']
         widgets = {
-            'dt_conclusao_prev': forms.DateInput(attrs={'type': 'date'})
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'conteudo': forms.Textarea(attrs={'class': 'form-control'}),
+            'progresso': forms.Select(attrs={'class': 'form-control'}),
+            'dt_conclusao_prev': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'responsavel': forms.Select(attrs={'class': 'form-control'})
         }
