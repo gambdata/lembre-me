389ab18583b6ffc4c7a2b6067be5d196da87320e
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 13:54:47 -0300

    Novas urls rotas incluidas, editar e delete

--- planner/urls.py
+++ planner/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket
+from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView
 
 urlpatterns = [
     path('criar/', NovoViewTicket.as_view(), name='novo-ticket'),
-    path('editar/<pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
-    path('excluir/<pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
+    path('editar/<int:pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
+    path('excluir/<int:pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
+    path('', TicketListView.as_view(), name='lista-ticket'),
 ]
