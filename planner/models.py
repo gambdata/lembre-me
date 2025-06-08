486ff863c46ce4f09fdfea321bf07c629467f4eb
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 13:54:22 -0300

    Alteração nas colunas criado_por e pogresso

--- planner/models.py
+++ planner/models.py
@@ -1,10 +1,13 @@
-from django.conf import settings
+# from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
 
 from users.models import Usuario
 
-PROGRESS = [
+User = get_user_model()
+
+PROGRESSO = [
     ('0', 'Novo'),
     ('1', 'Em andamento'),
     ('2', 'Pendente'),
@@ -13,10 +16,10 @@ PROGRESS = [
 ]
 
 class Ticket(models.Model):
-    criado_por = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='tickets_criados', default=Usuario)
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets_criados')
     titulo = models.CharField(_('Título'), max_length=50, null=False)
     conteudo = models.TextField(_('Conteúdo'))
-    progresso = models.CharField(_('Progresso'), max_length=1, choices=PROGRESS, default='0')
+    progresso = models.CharField(_('Progresso'), max_length=1, choices=PROGRESSO, default='0')
     dt_inicio = models.DateTimeField(_('Data início'), auto_now_add=True)
     dt_fim = models.DateTimeField(_('Data fim'), blank=True, null=True)
     dt_conclusao_prev = models.DateTimeField(_('Data conclusão prevista'), blank=True, null=True)
