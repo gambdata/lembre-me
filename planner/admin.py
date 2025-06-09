152ecd8159772226915629423056c859561d9ba2
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 23:58:48 -0300

    Incluído novo model Progresso

--- planner/admin.py
+++ planner/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Ticket
+from .models import Ticket, Progresso
 
 class CustomPlannerAdmin(admin.ModelAdmin):
     model = Ticket
     list_display = ['titulo', 'dt_inicio', 'progresso', 'responsavel']
 
 admin.site.register(Ticket, CustomPlannerAdmin)
+admin.site.register(Progresso)
