136ffa4cd1ccfe373af5eaf34e35a9a100439567
Paulo Martins <p.o.marttins@gmail.com>
2025-06-08 23:59:18 -0300

    Criação do model Progresso

--- planner/forms.py
+++ planner/forms.py
@@ -5,11 +5,12 @@ from .models import Ticket
 class NovoTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['titulo','conteudo','dt_conclusao_prev','responsavel']
+        fields = ['titulo','conteudo','progresso','dt_conclusao_prev','responsavel']
         widgets = {
             # 'criado_por': forms.Select(attrs={'class': 'form-control'}),
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'conteudo': forms.Textarea(attrs={'class': 'form-control'}),
+            'progresso': forms.Select(attrs={'class': 'form-control'}),
             'dt_conclusao_prev': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'responsavel': forms.Select(attrs={'class': 'form-control'})
         }
