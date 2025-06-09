a0783d7f535c1d167ebab128b5b5cadb763bd20f
Paulo Martins <p.o.marttins@gmail.com>
2025-06-09 00:00:11 -0300

    Incluído nova rota url ProjetoListView

--- planner/urls.py
+++ planner/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView
+from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView, ProjetosListView
 
 urlpatterns = [
     path('criar/', NovoViewTicket.as_view(), name='novo-ticket'),
     path('editar/<int:pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
     path('excluir/<int:pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
     path('', TicketListView.as_view(), name='lista-ticket'),
+    path('projetos/', ProjetosListView.as_view(), name='projetos-ticket'),
 ]
