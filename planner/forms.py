6f60bb42c3cf01fd851c1d6fa0f9c9e860b27252
Paulo Martins <p.o.marttins@gmail.com>
2025-06-10 17:46:40 -0300

    Novo form para o Subticket

--- planner/forms.py
+++ planner/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Ticket
+from .models import Ticket, SubTicket
 
 class NovoTicketForm(forms.ModelForm):
     class Meta:
@@ -18,11 +18,20 @@ class NovoTicketForm(forms.ModelForm):
 class UpdateTicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['titulo','conteudo','progresso','dt_conclusao_prev','responsavel']
+        fields = ['titulo','conteudo','progresso','dt_conclusao_prev','responsavel','subticket']
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control'}),
             'conteudo': forms.Textarea(attrs={'class': 'form-control'}),
             'progresso': forms.Select(attrs={'class': 'form-control'}),
             'dt_conclusao_prev': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'responsavel': forms.Select(attrs={'class': 'form-control'})
+            'responsavel': forms.Select(attrs={'class': 'form-control'}),
+            'subticket': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+class NovoSubTicketForm(forms.ModelForm):
+    class Meta:
+        model = SubTicket
+        fields = ['titulo', 'status']
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'})
         }
