5def821c9eb7d55c5eed563ef50079497df17fda
Paulo Martins <p.o.marttins@gmail.com>
2025-06-10 17:46:11 -0300

    Adicionado registro do SubTicket

--- planner/admin.py
+++ planner/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Ticket, Progresso
+from .models import SubTicket, Ticket, Progresso
 
 class CustomPlannerAdmin(admin.ModelAdmin):
     model = Ticket
     list_display = ['titulo', 'dt_inicio', 'progresso', 'responsavel']
+    list_filter = ['titulo', 'responsavel']
+
+@admin.register(SubTicket)
+class CustomPlannerSubTicket(admin.ModelAdmin):
+    list_display = ['criado_por', 'titulo', 'status']
+    list_filter = ['titulo']
 
 admin.site.register(Ticket, CustomPlannerAdmin)
 admin.site.register(Progresso)
