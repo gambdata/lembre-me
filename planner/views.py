38fa0a243260828768d6896c526c82ff77c3e517
Paulo Martins <p.o.marttins@gmail.com>
2025-06-11 12:00:56 -0300

    Criadas as views de Projeto: novo, update e lista

--- planner/views.py
+++ planner/views.py
@@ -4,24 +4,19 @@ from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 
-from .models import Ticket, SubTicket
-from .forms import NovoTicketForm, UpdateTicketForm, NovoSubTicketForm
+from .models import Ticket, Projeto
+from .forms import NovoTicketForm, UpdateTicketForm, NovoProjetoForm, UpdateProjetoForm
 
 class TicketListView(LoginRequiredMixin, ListView):
     model = Ticket
     template_name = 'planner/lista_ticket.html'
     paginate_by = 3
 
-class ProjetosListView(LoginRequiredMixin, ListView):
-    model = Ticket
-    template_name = 'planner/deck_projetos.html'
-    paginate_by = 10
-
 class NovoViewTicket(LoginRequiredMixin, CreateView):
     model = Ticket
     form_class = NovoTicketForm
     template_name = 'planner/novo_ticket.html'
-    success_url = '/planner/'
+    success_url = '/planner/tickets/'
     login_url = 'login'
 
     # Função para setar o usuário logado como criador
@@ -30,23 +25,11 @@ class NovoViewTicket(LoginRequiredMixin, CreateView):
             # form.instance.progresso = 'Novo'
             return super().form_valid(form)
 
-class NovoViewSubTicket(LoginRequiredMixin, CreateView):
-    model = SubTicket
-    form_class = NovoSubTicketForm
-    template_name = 'planner/novo_subticket.html'
-    success_url = '/planner/'
-    login_url = 'login'
-
-    def form_valid(self, form):
-        form.instance.criado_por = self.request.user
-        form.instance.status = 0
-        return super().form_valid(form)
-
 class UpdateViewTicket(LoginRequiredMixin, UpdateView):
     model = Ticket
     form_class = UpdateTicketForm
-    template_name = 'planner/ticket_update_form.html'
-    success_url = '/planner/'
+    template_name = 'planner/update_ticket.html'
+    success_url = '/planner/tickets/'
     login_url = 'login'
 
 class DeleteViewTicket(LoginRequiredMixin, DeleteView):
@@ -62,3 +45,28 @@ class DeleteViewTicket(LoginRequiredMixin, DeleteView):
             return HttpResponseRedirect(
                 reverse('excluir-ticket', kwargs={'pk': self.object.pk})
             )
+
+class ProjetosListView(LoginRequiredMixin, ListView):
+    model = Projeto
+    template_name = 'planner/lista_projeto.html'
+    paginate_by = 3
+
+class NovoViewProjeto(LoginRequiredMixin, CreateView):
+    model = Projeto
+    form_class = NovoProjetoForm
+    template_name = 'planner/novo_projeto.html'
+    success_url = '/planner/projetos/'
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.criado_por = self.request.user
+        form.instance.responsavel = self.request.user
+        form.instance.status = 0
+        return super().form_valid(form)
+
+class UpdateViewProjeto(LoginRequiredMixin, UpdateView):
+    model = Projeto
+    form_class = UpdateProjetoForm
+    template_name = 'planner/update_projeto.html'
+    success_url = '/planner/projetos/'
+    login_url = 'login'
