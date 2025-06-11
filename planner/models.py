ffa70c2f6923d788096fba20f3b417a6719bda2c
Paulo Martins <p.o.marttins@gmail.com>
2025-06-11 11:59:46 -0300

    Criado modelo Projeto

--- planner/models.py
+++ planner/models.py
@@ -21,18 +21,9 @@ class Progresso(models.Model):
     def __str__(self):
         return self.progresso
 
-class SubTicket(models.Model):
-    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subtickets_criados')
-    titulo = models.CharField(_('Titulo'), max_length=50, null=False)
-    status = models.BooleanField(_('Status'), default=0)
-
-    def __str__(self):
-        return self.titulo
-
 class Ticket(models.Model):
     criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets_criados')
     titulo = models.CharField(_('Título'), max_length=50, null=False)
-    conteudo = models.TextField(_('Conteúdo'))
     # progresso = models.CharField(_('Progresso'), max_length=1, choices=PROGRESSO, default='1')
     progresso = models.ForeignKey(Progresso, on_delete=models.CASCADE, related_name='ticket_progresso')
     dt_inicio = models.DateTimeField(_('Data início'), auto_now_add=True)
@@ -40,7 +31,19 @@ class Ticket(models.Model):
     dt_conclusao_prev = models.DateTimeField(_('Data conclusão prevista'), blank=True, null=True)
     dt_atualizacao = models.DateTimeField(_('Data atualização'), auto_now=True)
     responsavel = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='tickets_designados')
-    subticket = models.ManyToManyField(SubTicket, related_name='subticket_ticket')
+
+    def __str__(self):
+        return self.titulo
+
+class Projeto(models.Model):
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projeto_criado')
+    titulo = models.CharField(_('Titulo'), max_length=50, null=False)
+    descricao = models.TextField(_('Descrição'), blank=True, null=True)
+    status = models.BooleanField(_('Status'), default=0)
+    dt_inicio = models.DateTimeField(_('Data início'), auto_now_add=True)
+    dt_fim = models.DateTimeField(_('Data fim'), blank=True, null=True)
+    sub_ticket = models.ManyToManyField(Ticket, related_name='projeto_ticket')
+    responsavel = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='projeto_designado')
 
     def __str__(self):
         return self.titulo
