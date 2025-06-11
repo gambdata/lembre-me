5e735bfffd4d5f91d18a3dd547efc881f3457f9a
Paulo Martins <p.o.marttins@gmail.com>
2025-06-11 12:00:09 -0300

    Adicionadas novas rotas url para projetos

--- planner/urls.py
+++ planner/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import NovoViewSubTicket, NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView, ProjetosListView
+from .views import NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView, ProjetosListView, NovoViewProjeto, UpdateViewProjeto
 
 urlpatterns = [
-    path('criar/', NovoViewTicket.as_view(), name='novo-ticket'),
-    path('editar/<int:pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
-    path('excluir/<int:pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
-    path('', TicketListView.as_view(), name='lista-ticket'),
-    path('projetos/', ProjetosListView.as_view(), name='projetos-ticket'),
-    path('criar_subticket/', NovoViewSubTicket.as_view(), name='novo-subticket'),
+    path('tickets/criar/', NovoViewTicket.as_view(), name='novo-ticket'),
+    path('tickets/editar/<int:pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
+    path('tickets/excluir/<int:pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
+    path('tickets/', TicketListView.as_view(), name='lista-ticket'),
+    path('projetos/', ProjetosListView.as_view(), name='lista-projeto'),
+    path('projetos/criar/', NovoViewProjeto.as_view(), name='novo-projeto'),
+    path('projetos/editar/<int:pk>', UpdateViewProjeto.as_view(), name='editar-projeto'),
 ]
