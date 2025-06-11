ecf7900dd1e7012fc20169d4321eeef390f54fbc
Paulo Martins <p.o.marttins@gmail.com>
2025-06-11 11:58:23 -0300

    Atualização do admin(Projeto)

--- planner/admin.py
+++ planner/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
 
-from .models import SubTicket, Ticket, Progresso
+from .models import Ticket, Progresso, Projeto
 
-class CustomPlannerAdmin(admin.ModelAdmin):
-    model = Ticket
+@admin.register(Ticket)
+class CustomPlannerTicket(admin.ModelAdmin):
     list_display = ['titulo', 'dt_inicio', 'progresso', 'responsavel']
     list_filter = ['titulo', 'responsavel']
 
-@admin.register(SubTicket)
-class CustomPlannerSubTicket(admin.ModelAdmin):
-    list_display = ['criado_por', 'titulo', 'status']
+@admin.register(Projeto)
+class CustomPlannerProjeto(admin.ModelAdmin):
+    list_display = ['criado_por','titulo','dt_inicio','responsavel','status']
     list_filter = ['titulo']
 
-admin.site.register(Ticket, CustomPlannerAdmin)
 admin.site.register(Progresso)
