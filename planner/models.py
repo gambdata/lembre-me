f5ac418030a45b1cbf675ee37458db73e391c04f
Paulo Martins <p.o.marttins@gmail.com>
2025-06-24 02:03:17 -0300

    Atualização do model Progresso

--- planner/models.py
+++ planner/models.py
@@ -18,6 +18,9 @@ PROGRESSO = [
 class Progresso(models.Model):
     progresso = models.CharField(_('Progresso'), max_length=20, default='Novo')
 
+    class Meta:
+        unique_together = ('progresso',)
+
     def __str__(self):
         return self.progresso
 
@@ -25,7 +28,7 @@ class Ticket(models.Model):
     criado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets_criados')
     titulo = models.CharField(_('Título'), max_length=50, null=False)
     # progresso = models.CharField(_('Progresso'), max_length=1, choices=PROGRESSO, default='1')
-    progresso = models.ForeignKey(Progresso, on_delete=models.CASCADE, related_name='ticket_progresso')
+    progresso = models.ForeignKey(Progresso, on_delete=models.CASCADE, related_name='ticket_progresso') # progresso.ticket_progresso.all()
     dt_inicio = models.DateTimeField(_('Data início'), auto_now_add=True)
     dt_fim = models.DateTimeField(_('Data fim'), blank=True, null=True)
     dt_conclusao_prev = models.DateTimeField(_('Data conclusão prevista'), blank=True, null=True)
