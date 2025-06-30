d83ce882e1f6b8fc7fb1210876df4043b27335a0
Paulo Martins <p.o.marttins@gmail.com>
2025-06-30 00:42:12 -0300

    Nova rota url para detalhes

--- planner/urls.py
+++ planner/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import DeleteViewProjeto, NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView, ProjetosListView, NovoViewProjeto, UpdateViewProjeto
+from .views import DeleteViewProjeto, NovoViewTicket, UpdateViewTicket, DeleteViewTicket, TicketListView, ProjetosListView, NovoViewProjeto, UpdateViewProjeto, DetailViewTicket
 
 urlpatterns = [
     path('tickets/', TicketListView.as_view(), name='lista-ticket'),
     path('tickets/criar/', NovoViewTicket.as_view(), name='novo-ticket'),
     path('tickets/editar/<int:pk>', UpdateViewTicket.as_view(), name='editar-ticket'),
     path('tickets/excluir/<int:pk>', DeleteViewTicket.as_view(), name='excluir-ticket'),
+    path('tickets/detalhes/<int:pk>', DetailViewTicket.as_view(), name='detalhe-ticket'),
     path('projetos/', ProjetosListView.as_view(), name='lista-projeto'),
     path('projetos/criar/', NovoViewProjeto.as_view(), name='novo-projeto'),
     path('projetos/editar/<int:pk>', UpdateViewProjeto.as_view(), name='editar-projeto'),
