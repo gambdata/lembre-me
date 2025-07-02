6ab59ffb6b53da14bb12cc6bed6536de9656a547
Paulo Martins <p.o.marttins@gmail.com>
2025-07-01 23:28:06 -0300

    Coluna de dt_fim incluída no admin

--- planner/admin.py
+++ planner/admin.py
@@ -4,12 +4,12 @@ from .models import Ticket, Progresso, Projeto
 
 @admin.register(Ticket)
 class CustomPlannerTicket(admin.ModelAdmin):
-    list_display = ['titulo', 'dt_inicio', 'progresso', 'responsavel']
+    list_display = ['titulo', 'dt_inicio', 'dt_fim', 'progresso', 'responsavel']
     list_filter = ['titulo', 'responsavel']
 
 @admin.register(Projeto)
 class CustomPlannerProjeto(admin.ModelAdmin):
-    list_display = ['criado_por','titulo','dt_inicio','responsavel','status']
+    list_display = ['criado_por','titulo','dt_inicio','dt_fim','responsavel','status']
     list_filter = ['titulo']
 
 admin.site.register(Progresso)
